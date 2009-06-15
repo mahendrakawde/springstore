cc3a402349b821d93d524c6b60bc8e840272b04f
mdeinum <mdeinum@e78a211c-9125-0410-a6bc-1d1e04c0b581>
2009-06-15 11:40:11 +0000

    - removed generated sources and obsolete files - added source generation to pom

--- adventure-builder-enterprise/war/src/main/java/com/sun/j2ee/blueprints/consumerwebsite/actions/OrderTrackingService.java
+++
@@ -1,6 +0,0 @@
-package com.sun.j2ee.blueprints.consumerwebsite.actions;
-
-public interface OrderTrackingService {
-	   public OrderDetails getOrderDetails(String orderId) throws OrderNotFoundException;
-
-}
